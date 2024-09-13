d200b42bd18af27c14c849a92a46a7b3dcea24be
staaason <stanislav.karashchuk@gmail.com>
2024-09-13 16:08:11 +0300

    refactor: import `rbpf` and `ed25519` from git

--- rbpf/tests/elfs/data_section.rs
+++
@@ -1,7 +0,0 @@
-static mut VAL: u64 = 42;
-
-#[no_mangle]
-pub fn entrypoint() -> u64 {
-    unsafe { core::ptr::write_volatile(&mut VAL, 0); }
-    return 0;
-}
--- rbpf/tests/elfs/syscall_static.rs
+++
@@ -1,7 +0,0 @@
-mod syscalls;
-
-#[no_mangle]
-pub fn entrypoint() -> u64 {
-    unsafe { syscalls::log(b"foo\n".as_ptr(), 4); }
-    return 0;
-}
